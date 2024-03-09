183b6a3562f1439a9cc03ea489e00c22063813e3
samehhamdymuslim <124298532+samehhamdymuslim@users.noreply.github.com>
2024-03-09 14:29:39 +0200

    feat mobile api doctors details

--- app/Http/Controllers/Api/V1/Mobile/DoctorController.php
+++ app/Http/Controllers/Api/V1/Mobile/DoctorController.php
@@ -7,13 +7,35 @@ use App\Http\Controllers\Api\V1\BaseApiController;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\DoctorResource;
 use App\Http\Resources\UserResource;
+use App\Models\Doctor;
 use App\Repositories\Contracts\DoctorContract;
+use Illuminate\Http\JsonResponse;
 
 class DoctorController extends BaseApiController
 {
+    /**
+     * DoctorController constructor.
+     * @param DoctorContract $contract
+     */
     public function __construct(DoctorContract $contract)
     {
         parent::__construct($contract, DoctorResource::class);
         $this->defaultScopes = ['requestStatus' => DoctorRequestStatusConstants::APPROVED->value];
+    }
+
+    /**
+     *  get specified doctor full details
+     *
+     * @param Doctor $doctor
+     * @return JsonResponse
+     */
+    public function show(Doctor $doctor)
+    {
+        if (!$doctor->request_status?->is(DoctorRequestStatusConstants::APPROVED))
+        {
+            return $this->respondWithError('Doctor not found', 404);
+        }
+        $doctor->load('medicalSpecialities', 'city', 'attachments', 'academicDegree');
+        return $this->respondWithModel($doctor);
     }
 }
